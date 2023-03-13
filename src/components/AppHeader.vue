d5fe54f2f3a1261a5ad36d79ba49b33eea6bc844
nicola00 <nicolajobs00@gmail.com>
2023-03-13 15:13:44 +0100

    FEAT header complete add style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,8 +9,103 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+  <header>
+    <div class="container">
+      <div class="head">
+        <div class="logo">
+          <figure>
+            <img src="/land/img/logo-dark.png" alt="">
+          </figure>
+        </div>
+        <div class="list-head">
+          <ul>
+            <li>Home</li>
+            <li class="active">
+              Landing
+              <span>
+                <i class="fa-solid fa-chevron-down"></i>
+              </span>
+            </li>
+            <li>
+              Pagies
+              <span>
+                <i class="fa-solid fa-chevron-down"></i>
+              </span>
+            </li>
+            <li>
+              Docs
+              <span>
+                <i class="fa-solid fa-chevron-down"></i>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div class="search-bar">
+          <span>
+
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </span>
+          <button><i class="icon-head fa-brands fa-github"></i></button>
+          <button><i class="icon-head fa-solid fa-chart-gantt"></i></button>
+          <button><i class="icon-head fa-regular fa-user"></i></button>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
 
 <style lang="scss" scoped>
 @use '../style/variables.scss' as *;
+
+.head {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+
+  .logo {
+    flex-basis: 10%;
+  }
+
+  .list-head {
+    flex-direction: row;
+
+    ul {
+      display: flex;
+      gap: 20px;
+      align-items: center;
+      text-transform: uppercase;
+      font-size: 12px;
+      color: grey;
+      font-weight: 800;
+
+      span {
+        font-size: 8px;
+      }
+
+      .active {
+        color: $text-bg-purple;
+      }
+    }
+  }
+
+  .fa-magnifying-glass {
+    color: $text-grey;
+    margin: 0px 10px;
+
+  }
+
+  button {
+    border-radius: 8px;
+    padding: 10px;
+    margin: 0px 5px;
+    background-color: $bg-icon-head;
+    color: $text-bg-purple;
+  }
+
+
+
+
+}
 </style>
