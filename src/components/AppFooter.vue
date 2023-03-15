2fa9b6b8e1efe2c3376c21316665e7f71400c970
nicola00 <nicolajobs00@gmail.com>
2023-03-15 11:32:38 +0100

    FEAT footer section contact

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,8 +9,324 @@ export default {
 
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <section class="contact">
+            <div class="container">
+                <div class="row-foot">
+                    <div class="col-logo">
+                        <figure class="logo-light">
+                            <img src="/land/img/logo-light.png" alt="">
+                        </figure>
+                        <p>
+                            Start working with Landrik that can provide everything you need
+                            to generate awareness, drive traffic,connect.
+                        </p>
+                        <span class="icon-social-foot">
+                            <i class="fa-brands fa-facebook-f"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </span>
+                    </div>
+                    <div class="col-data">
+                        <ul>
+                            <li>
+                                <h4>
+                                    Company
+                                </h4>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    About us
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Services
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Team
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Pricing
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Project
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Careers
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Blog
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Login
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-links">
+                        <ul>
+                            <li>
+                                <h4>
+                                    Usefull Links
+                                </h4>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Term of Services
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Privacy Policy
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Documentation
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Changelog
+                                </a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-chevron-right"></i>
+                                <a href="#">
+                                    Components
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-newsletter">
+                        <h4>Newsletter</h4>
+                        <p>
+                            Sing up and receive the latest
+                            tips via email
+                        </p>
+                        <div class="mail">
+                            <p>Write your mail <span><i class="fa-solid fa-circle"></i></span></p>
+
+                            <input type="text" placeholder="Your email:">
+                            <i class="fa-regular fa-envelope"></i>
+
+                            <button class="btn-mail">
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="nav-footer">
+            <div class="container">
+                <div class="row-bottom-foot">
+                    <div class="col">
+                        <p>
+                            <i class="fa-regular fa-copyright"></i>
+                            2020 Landrik. Dseign with <i class="fa-solid fa-heart"></i>
+                            by Shreethemes.
+                        </p>
+                    </div>
+                    <ul>
+                        <li>
+                            <img src="/land/img/american-ex.png" alt="">
+                        </li>
+                        <li>
+                            <img src="/land/img/discover.png" alt="">
+                        </li>
+                        <li>
+                            <img src="/land/img/master-card.png" alt="">
+                        </li>
+                        <li>
+                            <img src="/land/img/paypal.png" alt="">
+                        </li>
+                        <li>
+                            <img src="/land/img/visa.png" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </footer>
+</template>
 
 <style lang="scss" scoped>
 @use '../style/variables.scss' as *;
+
+footer {
+    background-color: $bg-footer;
+
+    .contact,
+    .nav-footer {
+        padding: 50px 20px;
+
+    }
+}
+
+.row-foot {
+    display: flex;
+    gap: 30px;
+
+    .col-logo,
+    .col-data,
+    .col-links,
+    .col-newsletter {
+        flex-basis: calc(100% / 4);
+    }
+}
+
+.col-data {
+    display: flex;
+    justify-content: center;
+    color: white;
+
+    li {
+
+
+        i {
+            font-size: 10px;
+            padding-right: 5px;
+            color: $text-grey-light;
+        }
+
+        h4 {
+            padding-bottom: 15px;
+        }
+
+        a {
+            font-size: 12px;
+            color: $text-grey-light;
+        }
+    }
+}
+
+.col-links {
+    color: white;
+
+    li {
+
+
+        i {
+            font-size: 10px;
+            padding-right: 5px;
+            color: $text-grey-light;
+        }
+
+        h4 {
+            padding-bottom: 15px;
+        }
+
+        a {
+            font-size: 12px;
+            color: $text-grey-light;
+        }
+    }
+}
+
+.col-newsletter {
+    color: white;
+
+    p {
+        padding: 10px 0;
+        color: $text-grey-light;
+    }
+
+    .mail {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+
+        p {
+            color: $text-grey-light;
+
+            span {
+                color: rgb(252, 71, 71);
+                font-size: 5px;
+            }
+        }
+
+        .fa-envelope {
+            position: absolute;
+            bottom: 0;
+            top: 51px;
+            left: 8px;
+        }
+
+        input {
+            padding: 10px 10px 10px 30px;
+            border-radius: 5px;
+            background-color: #27314F;
+            color: white;
+            margin: 0px 0 5px 0;
+        }
+
+        .btn-mail {
+            border-radius: 5px;
+            background-color: #222E51;
+            color: #2D4FBF;
+            margin: 5px 0;
+            padding: 10px 0;
+            font-weight: 600;
+            box-shadow: 0px 2px #2D4FBF;
+        }
+    }
+}
+
+.col-logo {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    color: white;
+
+    .logo-light {
+        img {
+            max-width: 120px;
+        }
+    }
+
+    .icon-social-foot {
+
+        i {
+            border: 1px solid white;
+            padding: 7px 10px;
+            border-radius: 8px;
+            margin-right: 5px;
+            line-height: 20px;
+            aspect-ratio: 3/2;
+            font-size: 15px;
+        }
+    }
+}
+
+.nav-footer {
+    border-top: 1px solid #52525268;
+}
 </style>
